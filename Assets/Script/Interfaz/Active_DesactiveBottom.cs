49b99029d5f62092dfa8f85192a1c73a6a7c8336
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-05-05 16:52:04 +0200

    Game
    Game

---
+++ Assets/Script/Interfaz/Active_DesactiveBottom.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Active_DesactiveBottom : MonoBehaviour {
+    public GameObject[] objIn, objOut;
+    public void ActiveDesactive()
+    {
+        for (int i = 0; i < objOut.Length; i++)
+        {
+            objOut[i].SetActive(false);
+        }
+        for (int i = 0; i < objIn.Length; i++)
+        {
+            objIn[i].SetActive(true);
+        }
+    }
+}
---
+++ Assets/UltimateIsometricToolkit/Scripts/IsoController/SimpleIsoObjectController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using Assets.UltimateIsometricToolkit.Scripts.Core;
+using System;
+
+namespace UltimateIsometricToolkit.controller { 
+/// <summary>
+/// Simple continuous movement with WSAD/Arrow Keys movement.
+/// Note: This is an exemplary implementation. You may vary inputs, speeds, etc.
+/// </summary>
+
+	public class SimpleIsoObjectController : MonoBehaviour {
+
+		public float Speed = 10;
+		
+		private IsoTransform _isoTransform;
+        private Vector3 initialMousePosition;
+        Vector3 mouseposition;
+
+        void Awake()
+        {
+			//_isoTransform = this.GetOrAddComponent<IsoTransform>(); //avoids polling the IsoTransform component per frame
+		}
+
+        void Update()
+        {
+           
+           
+
+            //translate on isotransform
+            if (Input.GetMouseButton(0))
+            {
+                mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    initialMousePosition = mouseposition;
+                }
+
+
+                if (initialMousePosition != mouseposition)
+                {
+                    if (Math.Abs(mouseposition.x - initialMousePosition.x) > 2 && Math.Abs(mouseposition.y - initialMousePosition.y) > 2)
+                    {
+                        transform.Translate(new Vector3(-(mouseposition.x - initialMousePosition.x), -(mouseposition.y - initialMousePosition.y), 0) * Time.deltaTime * Speed);
+                    }
+                    else
+                    {
+                        if (Math.Abs(mouseposition.x - initialMousePosition.x) > 2)
+                        {
+                            transform.Translate(new Vector3(-(mouseposition.x - initialMousePosition.x), 0, 0) * Time.deltaTime * Speed);
+                        }
+                        if(Math.Abs(mouseposition.y - initialMousePosition.y) > 2)
+                        {
+                            transform.Translate(new Vector3(0, -(mouseposition.y - initialMousePosition.y), 0) * Time.deltaTime * Speed);
+                        }
+                    }
+                }
+                
+            }
+        }
+         
+	}
+}
