e1d71dccae0bf5d841305ea509922c24acefbdc9
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-05-08 19:28:16 +0200

    Construction
    He empezado a hacer lo de la construcción, he puesto que instancie el
    "objeto fantasma" de antes de empezar a construir

---
+++ Assets/MyScript/Construction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Construction : MonoBehaviour {
+    public GameObject prefab;
+    private Vector3 instantiatePoint;
+    private GameObject panelConstruccion;
+	// Use this for initialization
+	void Start () {
+        panelConstruccion = GameObject.Find("PanelConstruir");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnClickBuilding()
+    {
+        instantiatePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Instantiate(prefab, instantiatePoint, Quaternion.identity);
+        panelConstruccion.SetActive(false);
+    }
+}
