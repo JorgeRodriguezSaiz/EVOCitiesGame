687782d9b7f98bfff0c484d223294b2697e770d4
cristianbarra <cristianbarrafernandez@gmail.com>
2017-05-08 08:48:18 +0200

    Ajustes
    inicio de ajustes

---
+++ Assets/Script/Interfaz/VolumenFx.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VolumenFx : MonoBehaviour {
+    GameObject [] allAudioSources;
+    public GameObject size,imagen;
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        imagen.GetComponent<RectTransform>().anchorMax = new Vector2 (size.GetComponent<Scrollbar>().value,1);
+        CambiarVolumenFx();
+	}
+    public void CambiarVolumenFx()
+    {
+        allAudioSources = GameObject.FindGameObjectsWithTag("FX");
+        foreach (GameObject audioS in allAudioSources)
+        {
+            audioS.GetComponent<AudioSource>().volume = size.GetComponent<Scrollbar>().value;
+        }
+    }
+}
---
+++ Assets/Script/Interfaz/VolumenMusica.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VolumenMusica : MonoBehaviour {
+    GameObject [] allAudioSources;
+    public GameObject size,imagen;
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        imagen.GetComponent<RectTransform>().anchorMax = new Vector2 (size.GetComponent<Scrollbar>().value,1);
+        CambiarVolumen();
+	}
+    public void CambiarVolumen()
+    {
+        allAudioSources = GameObject.FindGameObjectsWithTag("Music");
+        foreach (GameObject audioS in allAudioSources)
+        {
+            audioS.GetComponent<AudioSource>().volume = size.GetComponent<Scrollbar>().value;
+        }
+    }
+}
