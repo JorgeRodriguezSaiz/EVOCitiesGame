ede30208f0b400135a4716cc03583ffe51d255fd
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-05-09 23:11:54 +0200

    problemico

--- Assets/MyScript/Construction.cs
+++ Assets/MyScript/Construction.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Construction : MonoBehaviour {
     public GameObject prefab;
     public GameObject instancia;
-    private Vector3 instantiatePoint;
     public Vector3 mousePos = new Vector3();
     public bool modoConstruccion = false;
     // Use this for initialization
@@ -19,14 +18,21 @@ public class Construction : MonoBehaviour {
         if (modoConstruccion)
         {
             instancia.transform.position = Vector3.MoveTowards(instancia.transform.position, mousePos, 100f*Time.deltaTime);
-            //modoConstruccion = false;
         }
     }
     public void OnClickBuilding()
     {
-        prefab.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .5f);
-        //instantiatePoint = Camera.main.ScreenToWorldPoint(mousePos);
         instancia = (GameObject)Instantiate(prefab, mousePos, Quaternion.identity);
+        instancia.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .5f);
         modoConstruccion = true;
+    }
+    public void OnClickAceptar()
+    {
+        modoConstruccion = false;
+    }
+    public void OnClickCancelar()
+    {
+        Destroy(instancia);
+        modoConstruccion = false;
     }
 }
