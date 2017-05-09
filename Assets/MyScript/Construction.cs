cd865edec91f62cdf7bb9f135c50bb8b7b623925
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-05-09 22:34:23 +0200

    Movimiento fantasma
    conseguido el movimiento siguiendo el ratón

--- Assets/MyScript/Construction.cs
+++ Assets/MyScript/Construction.cs
@@ -3,35 +3,30 @@ using System.Collections;
 
 public class Construction : MonoBehaviour {
     public GameObject prefab;
-    private GameObject instancia;
+    public GameObject instancia;
     private Vector3 instantiatePoint;
-    private GameObject panelConstruccion;
-    Event e = Event.current;
     public Vector3 mousePos = new Vector3();
     public bool modoConstruccion = false;
     // Use this for initialization
     void Start () {
-        panelConstruccion = GameObject.Find("PanelConstruir");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = Camera.main.nearClipPlane;
         if (modoConstruccion)
         {
-            /*mousePos.x = Input.mousePosition.x;
-            mousePos.y = Input.mousePosition.y;
-            mousePos.z = Camera.main.nearClipPlane;*/
-            //instancia.transform.Translate(Camera.main.ScreenToWorldPoint(mousePos));
-            instancia.transform.position = Vector3.MoveTowards(transform.position, e.mousePosition, 1*Time.deltaTime);
+            instancia.transform.position = Vector3.MoveTowards(instancia.transform.position, mousePos, 100f*Time.deltaTime);
             //modoConstruccion = false;
         }
     }
     public void OnClickBuilding()
     {
         prefab.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .5f);
-        instantiatePoint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
-        instancia = (GameObject)Instantiate(prefab, instantiatePoint, Quaternion.identity);
+        //instantiatePoint = Camera.main.ScreenToWorldPoint(mousePos);
+        instancia = (GameObject)Instantiate(prefab, mousePos, Quaternion.identity);
         modoConstruccion = true;
     }
 }
