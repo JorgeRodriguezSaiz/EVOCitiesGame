bd1279101a3db08371fd125bf4e79d687091e38c
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-05-10 19:39:57 +0200

    Snap
    En principio el snap está hecho, queda preguntar que les parece

--- Assets/MyScript/Construction.cs
+++ Assets/MyScript/Construction.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Construction : MonoBehaviour {
     public GameObject prefab;
     public GameObject instancia;
+    public GameObject botonAceptar;
     public Vector3 mousePos = new Vector3();
+    public Vector3 mousePosCanvas = new Vector3();
     public bool modoConstruccion = false;
     // Use this for initialization
     void Start () {
@@ -14,10 +16,15 @@ public class Construction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.x = Mathf.Round(mousePos.x);
+        mousePos.y = Mathf.Round(mousePos.y);
         mousePos.z = Camera.main.nearClipPlane;
+        mousePosCanvas = Input.mousePosition;
+        mousePosCanvas.z = Camera.main.nearClipPlane;
         if (modoConstruccion)
         {
             instancia.transform.position = Vector3.MoveTowards(instancia.transform.position, mousePos, 100f*Time.deltaTime);
+            botonAceptar.transform.position = Vector3.MoveTowards(botonAceptar.transform.position, Input.mousePosition, 10000f * Time.deltaTime);
         }
     }
     public void OnClickBuilding()
@@ -29,6 +36,7 @@ public class Construction : MonoBehaviour {
     public void OnClickAceptar()
     {
         modoConstruccion = false;
+        instancia.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
     }
     public void OnClickCancelar()
     {
