d3d1eb111c0d6205829a8a2c0ef0c1133f895c1b
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-05-12 18:18:28 +0200

    Movimiento Camara modo construccion
    Terminado movimiento camara en modo construccion

--- Assets/UltimateIsometricToolkit/Scripts/Examples/SmoothCamera2d.cs
+++ Assets/UltimateIsometricToolkit/Scripts/Examples/SmoothCamera2d.cs
@@ -17,44 +17,75 @@ public class SmoothCamera2d : MonoBehaviour
     private Vector3 initialMousePosition;
     Vector3 mouseposition;
     //derecha y arriba true, izquierda y abajo false
-    private bool aux;
+    public bool _modoConstruction;
     public float smoothTime = 0.15F;
+    public GameObject controller;
+    public Vector3 _mousePos = new Vector3();
+    public float velocidadCamaraConstruccion = 10f;
+    public float distanciaHastaMover = 5f;
 
     void Start()
     {
+        controller = GameObject.Find("Controller");
         target = this.transform;
     }
     // Update is called once per frame
     void Update()
     {
+        _modoConstruction = controller.GetComponent<Construction>().modoConstruccion;
+        _mousePos = controller.GetComponent<Construction>().mousePos;
         if (target)
         {
             float targetX = target.position.x;
             float targetY = target.position.y;
             if (targetX >= minX && targetX <= maxY && targetY >= minY && targetY <= maxY)
             {
-                move();
+               if (_modoConstruction)
+               {
+                    if (_mousePos.x > transform.position.x + distanciaHastaMover)
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position, _mousePos, velocidadCamaraConstruccion * Time.deltaTime);
+                    }
+                    if (_mousePos.x < transform.position.x - distanciaHastaMover)
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position, _mousePos, velocidadCamaraConstruccion * Time.deltaTime);
+                    }
+                    if (_mousePos.y > transform.position.y + distanciaHastaMover)
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position, _mousePos, velocidadCamaraConstruccion * Time.deltaTime);
+                    }
+                    if (_mousePos.y < transform.position.y - distanciaHastaMover)
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position, _mousePos, velocidadCamaraConstruccion * Time.deltaTime);
+                    }
+                }
+               else
+               {
+                  move();
+               }
             }
 
 
-            if (targetX < minX)
-            {
-                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(minX + 0.01f, targetY, -10), ref velocity, smoothTime);
-            }
-            if (targetX > maxX)
-            {
-                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(maxX - 0.01f, targetY, -10), ref velocity, smoothTime);
-            }
-            if (targetY < minY)
-            {
-                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetX, minY + 0.01f, -10), ref velocity, smoothTime);
-            }
-            if (targetY > maxY)
-            {
-                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetX, maxY - 0.01f, -10), ref velocity, smoothTime);
-            }
+                if (targetX < minX)
+                {
+                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(minX + 0.01f, targetY, -10), ref velocity, smoothTime);
+                    
+                }
+                if (targetX > maxX)
+                {
+                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(maxX - 0.01f, targetY, -10), ref velocity, smoothTime);
+                }
+                if (targetY < minY)
+                {
+                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetX, minY + 0.01f, -10), ref velocity, smoothTime);
+                }
+                if (targetY > maxY)
+                { 
+                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetX, maxY - 0.01f, -10), ref velocity, smoothTime);
+                }
         }
-    }
+}
+    
     void move()
     {
         if (Input.GetMouseButton(0))
