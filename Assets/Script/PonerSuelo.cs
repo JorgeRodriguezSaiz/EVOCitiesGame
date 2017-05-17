04743839d297a5ee17a3387b0cfe89a8990808b3
cristianbarra <cristianbarrafernandez@gmail.com>
2017-05-17 18:24:03 +0200

    V.0.0.8

---
+++ Assets/Script/PonerSuelo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Assets.UltimateIsometricToolkit.Scripts.Core
+{
+    [ExecuteInEditMode]
+    public class PonerSuelo : MonoBehaviour {
+        public float maxX, maxZ, xActual, zActual;
+        public GameObject instanciar;
+
+        // Use this for initialization
+
+        void Update()
+        {
+            for (int i = 0; i <= maxX; i++)
+            {
+
+                for (int j = 0; j <= maxZ; j++)
+                {
+                    Instantiate(instanciar);
+                    instanciar.GetComponent<IsoTransform>().Position = new Vector3(i, 0, j);
+                    instanciar.name = "suelo" + i +"" + j;
+                    //instanciar.transform.position = new Vector3(i, 0, j);
+                }
+            }
+        }
+    }
+}
