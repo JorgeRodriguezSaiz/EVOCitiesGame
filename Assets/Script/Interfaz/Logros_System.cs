a658d3c750e4eca0f2448a9165264e011f902330
cristianbarra <cristianbarrafernandez@gmail.com>
2017-05-22 09:43:35 +0200

    Arreglo logors

--- Assets/Script/Interfaz/Logros_System.cs
+++ Assets/Script/Interfaz/Logros_System.cs
@@ -5,26 +5,35 @@ public class Logros_System : MonoBehaviour {
     public GameObject[] objIn, objOut;
     public GameObject camara;
     public bool camaraMovimiento;
-    void OnMouseUp()
+    private float timeIni,timeFin;
+    private void OnMouseDown()
     {
-        Debug.Log("a");
-        for (int i = 0; i < objOut.Length; i++)
+        timeIni = Time.time;
+    }
+    private void OnMouseUpAsButton()
+    {
+        timeFin = Time.time;
+        if ( timeFin - timeIni < 0.1f)
         {
-            objOut[i].SetActive(false);
-        }
-        for (int i = 0; i < objIn.Length; i++)
-        {
-            objIn[i].SetActive(true);
-        }
-        if (camaraMovimiento)
-        {
-            camara.GetComponent<SmoothCamera2d>().enabled = true;
-            camara.GetComponent<PinchZoom>().enabled = true;
-        }
-        else
-        {
-            camara.GetComponent<SmoothCamera2d>().enabled = false;
-            camara.GetComponent<PinchZoom>().enabled = false;
+            Debug.Log("a");
+            for (int i = 0; i < objOut.Length; i++)
+            {
+                objOut[i].SetActive(false);
+            }
+            for (int i = 0; i < objIn.Length; i++)
+            {
+                objIn[i].SetActive(true);
+            }
+            if (camaraMovimiento)
+            {
+                camara.GetComponent<SmoothCamera2d>().enabled = true;
+                camara.GetComponent<PinchZoom>().enabled = true;
+            }
+            else
+            {
+                camara.GetComponent<SmoothCamera2d>().enabled = false;
+                camara.GetComponent<PinchZoom>().enabled = false;
+            }
         }
     }
 }
