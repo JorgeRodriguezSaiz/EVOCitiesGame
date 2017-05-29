42e31f456dc2c050410eac5808aa623678f58c47
cristianbarra <cristianbarrafernandez@gmail.com>
2017-05-29 08:48:35 +0200

    Exp

--- Assets/MyScript/TimeController.cs
+++ Assets/MyScript/TimeController.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 public class TimeController : MonoBehaviour {
     public DateTime tiempoActual;
     public DateTime tiempoDesconexion, tiempoFinal;
-    public Text texto;
     private bool funcionar;
-	// Use this for initialization
-	void Start () {
-        //texto = GameObject.Find("Timer").GetComponent<Text>();
+
+	private void Start ()
+    {
         if(ZPlayerPrefs.HasKey("Tiempo " + GetInstanceID())){
             tiempoActual = Convert.ToDateTime( ZPlayerPrefs.GetString("Tiempo " + GetInstanceID()));
         }
@@ -25,17 +24,10 @@ public class TimeController : MonoBehaviour {
         {
             funcionar = true;
         }
-        //tiempoRestante = tiempoActual - tiempoDesconexion;
-        //texto.text = tiempoActual.ToString();
-        //DateTime tiempo = DateTime.ParseExact(tiempoRestante, "ddMMyyyy", CultureInfo.InvariantCulture);
-        //tiempoRestante.ToString();
-        Debug.Log(tiempoActual);
-        Debug.Log(tiempoFinal);
-        Debug.Log(tiempoDesconexion);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Update ()
+    {
         tiempoDesconexion = DateTime.Now;
         if (tiempoDesconexion >= tiempoFinal)
         {
