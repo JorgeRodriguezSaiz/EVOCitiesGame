94b86b750d460f4eafc3f931a8dc3b3d066de1f1
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-05-31 22:11:25 +0200

    hjghhg

--- Assets/MyScript/Casa.cs
+++ Assets/MyScript/Casa.cs
@@ -1,64 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+namespace Assets.UltimateIsometricToolkit.Scripts.Core
+{
+    public class Casa : MonoBehaviour
+    {
+        [Header("Experiencia")]
+        public float exp = 75f;
+        public float poblacionCasa = 2;
+        public TimeSpan tiempoRestante;
+        public DateTime tiempoActual;
+        public DateTime tiempoDesconexion, tiempoFinal;
+        public bool funcionar = false;
+        // Use this for initialization
+        void Start()
+        {
+            //ZPlayerPrefs.DeleteAll();
 
-public class Casa : MonoBehaviour {
-    [Header ("Experiencia")]
-    public float exp = 75f;
-    public float poblacionCasa = 2;
-    public TimeSpan tiempoRestante;
-    public DateTime tiempoActual;
-    public DateTime tiempoDesconexion, tiempoFinal;
-    public bool funcionar = false;
-    // Use this for initialization
-    void Start () {
-        //ZPlayerPrefs.DeleteAll();
-       
-        if (!funcionar)
-        {
-            if (ZPlayerPrefs.HasKey("Tiempo " + GetInstanceID()))
-            {
-                tiempoActual = Convert.ToDateTime(ZPlayerPrefs.GetString("Tiempo " + GetInstanceID()));
-            }
-            else
-            {
-                tiempoActual = DateTime.Now;
-                ZPlayerPrefs.SetString("Tiempo " + GetInstanceID(), tiempoActual.ToString());
-            }
-            //tiempoFinal = tiempoActual.AddMinutes(5D);
-            tiempoFinal = tiempoActual.AddSeconds(20D);
-            tiempoDesconexion = DateTime.Now;
-            tiempoRestante = tiempoFinal - tiempoDesconexion;
-        }
-        if (tiempoDesconexion >= tiempoFinal)
-        {
-            gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            funcionar = true;
-            this.enabled = false;
-        }
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        tiempoDesconexion = DateTime.Now;
-        if (!GameObject.Find("Controller").GetComponent<Construction>().modoConstruccion)
-        {
-            if (tiempoDesconexion >= tiempoFinal)
-            {
-                funcionar = true;
-                gameObject.transform.GetChild(0).gameObject.SetActive(false);
-                GameObject.Find("God").GetComponent<Exp_controller>().exp += this.exp;
-                GameObject.Find("Controller").GetComponent<Construction>().poblacion += poblacionCasa;
-                this.enabled = false;
-                
-            }
             if (!funcionar)
             {
-                string aux = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D2}", tiempoRestante.Days, tiempoRestante.Hours, tiempoRestante.Minutes, tiempoRestante.Seconds);
-                gameObject.GetComponentInChildren<TextMesh>().text = aux;
-                float tAux = (float)tiempoRestante.TotalSeconds;
-                tAux -= 1 * Time.deltaTime;
-                tiempoRestante = TimeSpan.FromSeconds(tAux);
+                if (ZPlayerPrefs.HasKey("Tiempo " + GetInstanceID()))
+                {
+                    tiempoActual = Convert.ToDateTime(ZPlayerPrefs.GetString("Tiempo " + GetInstanceID()));
+                }
+                else
+                {
+                    tiempoActual = DateTime.Now;
+                    ZPlayerPrefs.SetString("Tiempo " + GetInstanceID(), tiempoActual.ToString());
+                }
+                //tiempoFinal = tiempoActual.AddMinutes(5D);
+                tiempoFinal = tiempoActual.AddSeconds(20D);
+                tiempoDesconexion = DateTime.Now;
+                tiempoRestante = tiempoFinal - tiempoDesconexion;
+            }
+            if (tiempoDesconexion >= tiempoFinal)
+            {
+                gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                funcionar = true;
+                this.enabled = false;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            tiempoDesconexion = DateTime.Now;
+            if (!GameObject.Find("Controller").GetComponent<Construction>().modoConstruccion)
+            {
+                if (tiempoDesconexion >= tiempoFinal)
+                {
+                    funcionar = true;
+                    gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                    GameObject.Find("God").GetComponent<Exp_controller>().exp += this.exp;
+                    GameObject.Find("Controller").GetComponent<Construction>().poblacion += poblacionCasa;
+                    this.enabled = false;
+
+                }
+                if (!funcionar)
+                {
+                    string aux = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D2}", tiempoRestante.Days, tiempoRestante.Hours, tiempoRestante.Minutes, tiempoRestante.Seconds);
+                    gameObject.GetComponentInChildren<TextMesh>().text = aux;
+                    float tAux = (float)tiempoRestante.TotalSeconds;
+                    tAux -= 1 * Time.deltaTime;
+                    tiempoRestante = TimeSpan.FromSeconds(tAux);
+                }
             }
         }
     }
--- Assets/MyScript/comprobadorDisponible.cs
+++ Assets/MyScript/comprobadorDisponible.cs
@@ -1,32 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
-[RequireComponent(typeof(Collider2D))]
+namespace Assets.UltimateIsometricToolkit.Scripts.Core
+{
+    [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
-public class comprobadorDisponible : MonoBehaviour {
-    public GameObject controlador;
-    public bool _disponible = true;
-	// Use this for initialization
-	void Start () {
-        controlador = GameObject.Find("Controller");
-        _disponible = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-     _disponible = controlador.GetComponent<Construction>().disponible;
-    }
-    void OnTriggerEnter2D (Collider2D coll)
-    {
-        Debug.Log("Entra en el trigger");
-        //_instancia.GetComponent<SpriteRenderer>().color = Color.red;
-        controlador.GetComponent<Construction>().instancia.GetComponent<SpriteRenderer>().color = new Color(1f, 0f, 0f, .5f);
-        _disponible = false;
-    }
-    void OnTriggerExit2D(Collider2D coll)
-    {
-        Debug.Log("Sale del trigger");
-        //_instancia.GetComponent<SpriteRenderer>().color = Color.white;
-        controlador.GetComponent<Construction>().instancia.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .5f);
-        _disponible = true;
+
+    public class comprobadorDisponible : MonoBehaviour {
+        public GameObject controlador;
+        public bool _disponible = true;
+        // Use this for initialization
+        void Start() {
+            controlador = GameObject.Find("Controller");
+            _disponible = true;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            _disponible = controlador.GetComponent<Construction>().disponible;
+        }
+        void OnTriggerEnter2D(Collider2D coll)
+        {
+            Debug.Log("Entra en el trigger");
+            //_instancia.GetComponent<SpriteRenderer>().color = Color.red;
+            controlador.GetComponent<Construction>().instancia.GetComponent<SpriteRenderer>().color = new Color(1f, 0f, 0f, .5f);
+            _disponible = false;
+        }
+        void OnTriggerExit2D(Collider2D coll)
+        {
+            Debug.Log("Sale del trigger");
+            //_instancia.GetComponent<SpriteRenderer>().color = Color.white;
+            controlador.GetComponent<Construction>().instancia.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .5f);
+            _disponible = true;
+        }
     }
 }
