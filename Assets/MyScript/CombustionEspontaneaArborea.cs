b7015774285e21ed09afa9913c6e40206f6dd9b5
JorgeRodriguezSaiz <koke.rodriguez96@gmail.com>
2017-06-12 11:04:16 +0200

    Pete

---
+++ Assets/MyScript/CombustionEspontaneaArborea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombustionEspontaneaArborea : MonoBehaviour {
+    public GameObject panelSeguro;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnMouseDown()
+    {
+        panelSeguro.SetActive(true);
+    }
+}
